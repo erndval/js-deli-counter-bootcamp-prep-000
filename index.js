b5f9d6ac995262c343c4d61bdb31df87a364ff55
Ernie Val <erndval@gmail.com>
2019-03-01 21:47:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,9 @@ function nowServing (line) {
 }
 
 function currentLine(line) {
-  
+  if (line.length === 0) {
+    return "The line is currently empty."
+  } else {
+    return "The line is currently: "
+  }
 }
