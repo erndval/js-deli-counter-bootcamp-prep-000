04c65b5bd1d65fdeb3fefd6585f3525708c1e7a0
Ernie Val <erndval@gmail.com>
2019-03-01 22:06:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line) {
   } else {
     var neatList = []
     for (var i = 0; i < line.length; i++) {
-      neatList = [...neatList, ` ${i+1}. ${line[i]}`]
+      neatList = [...neatList,  `${i+1}. ${line[i]}`]
     }
     return `The line is currently: ${[neatList]}`
   }
