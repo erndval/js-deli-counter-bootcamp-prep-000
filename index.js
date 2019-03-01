103f05f5c32daa7788dfbfac8243eb810b4d87ee
Ernie Val <erndval@gmail.com>
2019-03-01 21:30:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function takeANumber(line, name) {
+<<<<<<< HEAD
   return `Welcome, ${name}. You are number ${line.length + 1} in line.`
+=======
+  return `Welcome ${name}. You are number ${line.length + 1} in line.`
+>>>>>>> 047bee81721e9b8cf080ab7fbdee907c6abe3133
 }
