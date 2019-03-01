aa7518d30fb6ecb3fa546634e2a19218134b9967
Ernie Val <erndval@gmail.com>
2019-03-01 21:53:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
   } else {
-    for ()
+    neatList = []
+    for (var i = 0; i < line.length; i++) {
+      
+    }
     return "The line is currently: "
   }
 }
